f87b3fdb02150d690693226f1f233364eba98c05
Christian <chrisadrianotav@gmail.com>
2020-10-19 01:32:51 -0400

    add portfolio to img links

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,14 +5,14 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 const previewsOne = [
-    { preview: "Credibility Capital", path: "http://www.credibilitycapital.com", img: "/credcap-icon.png", border: '50%', rounded: true },
-    { preview: "Project Innovate Newark", path: "http://www.projectinnovatenewark.org", img: "/pin-copy.png", border: '50%', rounded: false },
-    { preview: "The Todo App", path: "https://github.com/Christiantav/todo-app", img: "/todo.png", border: '50%', rounded: true },
+    { preview: "Credibility Capital", path: "http://www.credibilitycapital.com", img: "/portfolio/credcap-icon.png", border: '50%', rounded: true },
+    { preview: "Project Innovate Newark", path: "http://www.projectinnovatenewark.org", img: "/portfolio/pin-copy.png", border: '50%', rounded: false },
+    { preview: "The Todo App", path: "https://github.com/Christiantav/todo-app", img: "/portfolio/todo.png", border: '50%', rounded: true },
 ]
 
 const previewsTwo = [
-    { preview: "Blogger", path: "https://github.com/Christiantav/blogger", img: "/blogger.png", border: '50%', rounded: true },
-    { preview: "Computer Science Program Teacher", path: "https://github.com/projectinnovatenewark/csx", img: "/csx.png", border: '50%', rounded: false },
+    { preview: "Blogger", path: "https://github.com/Christiantav/blogger", img: "/portfolio/blogger.png", border: '50%', rounded: true },
+    { preview: "Computer Science Program Teacher", path: "https://github.com/projectinnovatenewark/csx", img: "/portfolio/csx.png", border: '50%', rounded: false },
 ]
 
 function Landing() {
