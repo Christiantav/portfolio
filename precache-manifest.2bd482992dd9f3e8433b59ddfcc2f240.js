1f2d11d0fb2d47f7dcc506f22c1ae787e88ee6f0
Christian <chrisadrianotav@gmail.com>
2020-10-19 01:01:53 -0400

    Updates

---
+++ precache-manifest.2bd482992dd9f3e8433b59ddfcc2f240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706af94e8b1519d3dc1027cdce91f64e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fb3ffadb21943a4112db",
+    "url": "/portfolio/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "26ed7ab023f289b33475",
+    "url": "/portfolio/static/css/main.fd8b022b.chunk.css"
+  },
+  {
+    "revision": "fb3ffadb21943a4112db",
+    "url": "/portfolio/static/js/2.3cb34576.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.3cb34576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ed7ab023f289b33475",
+    "url": "/portfolio/static/js/main.4b4d59e1.chunk.js"
+  },
+  {
+    "revision": "c952b3ed8759c3a5448e",
+    "url": "/portfolio/static/js/runtime-main.cd1dc1f4.js"
+  }
+]);
