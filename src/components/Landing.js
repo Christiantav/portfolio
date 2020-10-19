34d7988fddcbe618f7e7138169ba984afafc8825
Christian <chrisadrianotav@gmail.com>
2020-10-19 14:08:29 -0400

    update padding for imgs

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,13 +5,13 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 const previewsOne = [
-    { preview: "Credibility Capital", path: "http://www.credibilitycapital.com", img: "/portfolio/credcap-icon.png", border: '50%', rounded: true },
-    { preview: "Project Innovate Newark", path: "http://www.projectinnovatenewark.org", img: "/portfolio/pin-copy.png", border: '50%', rounded: false },
+    { preview: "Software Engineer @ Credibility Capital", path: "http://www.credibilitycapital.com", img: "/portfolio/credcap-icon.png", border: '50%', rounded: true },
+    { preview: "Lead Developer @ Project Innovate Newark", path: "http://www.projectinnovatenewark.org", img: "/portfolio/pin-copy.png", border: '50%', rounded: false },
     { preview: "The Todo App", path: "https://github.com/Christiantav/todo-app", img: "/portfolio/todo.png", border: '50%', rounded: true },
 ]
 
 const previewsTwo = [
-    { preview: "Blogger", path: "https://github.com/Christiantav/blogger", img: "/portfolio/blogger.png", border: '50%', rounded: true },
+    { preview: "The Blogger App", path: "https://github.com/Christiantav/blogger", img: "/portfolio/blogger.png", border: '50%', rounded: true },
     { preview: "Computer Science Program Teacher", path: "https://github.com/projectinnovatenewark/csx", img: "/portfolio/csx.png", border: '50%', rounded: false },
 ]
 
